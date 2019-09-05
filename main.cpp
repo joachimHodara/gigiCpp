b5ebccc8003cf5a60e3ea0b77e60c2be4031b6c7
Joachim Hodara <joachim.hodara@gmail.com>
2019-09-05 22:44:30 +0200

    constructors and destructors

--- main.cpp
+++ main.cpp
@@ -8,7 +8,20 @@ public:
     Vec2(float x = 0, float y = 0)
     : x_(x), y_(y)
     {
+        std::cout << "I just created a Vec2(x,y)" << std::endl;
         counter_++;
+    }
+
+    Vec2(const Vec2& u)
+    : x_(u.x_), y_(u.y_)
+    {
+        std::cout << "I just created a Vec2(u)" << std::endl;
+        counter_++;
+    }
+
+    ~Vec2()
+    {
+        std::cout << "I just destroyed a Vec2" << std::endl;
     }
 
     float getx() const
@@ -45,6 +58,11 @@ public:
         return Vec2(x_ + u.x_, y_ + u.y_);
     }
 
+    float dot(const Vec2& u) const
+    {
+        return x_*u.x_ + y_*u.y_;
+    }
+
     static int howMany() { return counter_; }
 
 private:
@@ -60,17 +78,23 @@ std::ostream& operator<<(std::ostream& out, const Vec2& v)
     return out;
 }
 
+float dot(const Vec2& u, const Vec2& v)
+{
+    return u.getx()*v.getx() + u.gety()*v.gety();
+}
+
+void print(Vec2 v)
+{
+    std::cout << v << std::endl;
+}
+
 int main()
 {
     Vec2 v(2,3);
-    Vec2 u(3);
+    Vec2 u(v);
 
-    std::cout << Vec2::howMany() << std::endl;
+    print(u);
 
-    Vec2 w = v + u;
-    std::cout << w << std::endl;
-
-    w += u;
-    std::cout << w << std::endl;
+    std::cout << "bonjour Bibounde" << std::endl;
 }
 
