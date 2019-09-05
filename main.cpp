55b61ac0dd9b6cb47c7f311801384cde29b454ef
Joachim Hodara <joachim.hodara@gmail.com>
2019-09-05 07:33:43 +0200

    Vec2 with print and operator+

---
+++ main.cpp
@@ -0,0 +1,67 @@
+#include <cassert>
+#include <fstream>
+#include <iostream>
+
+class Vec2
+{
+public:
+    Vec2(float x = 0, float y = 0)
+    : x_(x), y_(y)
+    {}
+
+    float getx() const
+    {
+        return x_;
+    }
+
+    float gety() const
+    {
+        return y_;
+    }
+
+    float operator[](int i) const
+    {
+        assert(i < 2);
+        return (i == 0) ? x_ : y_;
+    }
+
+    float& operator[](int i)
+    {
+        assert(i < 2);
+        return (i == 0) ? x_ : y_;
+    }
+
+    Vec2& operator+=(const Vec2& u)
+    {
+        x_ += u.x_;
+        y_ += u.y_;
+        return *this;
+    }
+
+    Vec2 operator+(const Vec2& u) const
+    {
+        return Vec2(x_ + u.x_, y_ + u.y_);
+    }
+
+private:
+    float x_, y_;
+};
+
+std::ostream& operator<<(std::ostream& out, const Vec2& v)
+{
+    out << '(' << v.getx() << ',' << v.gety() << ')';
+    return out;
+}
+
+int main()
+{
+    Vec2 v(2,3);
+    Vec2 u(3);
+
+    Vec2 w = v + u;
+    std::cout << w << std::endl;
+
+    w += u;
+    std::cout << w << std::endl;
+}
+
